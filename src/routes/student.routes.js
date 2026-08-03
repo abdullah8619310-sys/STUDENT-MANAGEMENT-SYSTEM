207e06dc8ead4cb4bc03eff8bdd56f90174fbde1
Muhammad Abdullah <abdullah8619310@gmail.com>
2026-08-03 17:47:37 +0500

    feat: add zod input validation for student routes

--- src/routes/student.routes.js
+++ src/routes/student.routes.js
@@ -7,16 +7,42 @@ import {
   deleteStudent,
 } from "../controllers/student.controller.js";
 
+import { validate } from "../middlewares/validate.js";
+
+import {
+  createStudentSchema,
+  updateStudentSchema,
+} from "../validators/student.validator.js";
+
+
 const router = Router();
 
-router.post("/", createStudent);
+router.post(
+  "/",
+  validate(createStudentSchema),
+  createStudent
+);
 
-router.get("/", getAllStudents);
+router.get(
+  "/",
+  getAllStudents
+);
 
-router.get("/:id", getStudentById);
+router.get(
+  "/:id",
+  getStudentById
+);
 
-router.put("/:id", updateStudent);
+router.put(
+  "/:id",
+  validate(updateStudentSchema),
+  updateStudent
+);
 
-router.delete("/:id", deleteStudent);
+router.delete(
+  "/:id",
+  deleteStudent
+);
+
 
 export default router;
